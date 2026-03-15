1bd408203af05db2db7c5d9ff7a6a5965812fc19
WH6489 <williamhuang129@yahoo.com>
2026-03-15 12:12:39 -0700

    Initial Commit

---
+++ Respite 2.0/Respite_2_0App.swift
@@ -0,0 +1,17 @@
+//
+//  Respite_2_0App.swift
+//  Respite 2.0
+//
+//  Created by William Huang on 2026-03-15.
+//
+
+import SwiftUI
+
+@main
+struct Respite_2_0App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
